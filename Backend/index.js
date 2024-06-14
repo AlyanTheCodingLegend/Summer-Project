dbb351726bddec083624253582ead011cb5400ff
VohraAK <abdullah.k.vohra@gmail.com>
2024-06-14 17:11:17 +0500

    made ws-setup branch

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,38 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { WebSocket, WebSocketServer }from 'ws';
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+const PORT = process.env.PORT;
 
 // add corsOptions here
 
-
-
-app.listen(3000, () => { console.log("Server is running on port 3000") });
+const server = app.listen(PORT, () => { console.log("Server is running on port 3000") });
 
 // add app routers here
 
+
+// initialise ws server
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => { 
+    console.log('New client connected!'); 
+
+    ws.on('message', (message) => 
+    {
+        console.log(`Received: ${message}`);
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {client.send(message)}    
+        });
+    });
+
+    ws.on('close', () => console.log("Client disconnected!"))
+});
 
 // error middleware (use in catch blocks in endpoints)
 app.use((error, request, response, next) => {
