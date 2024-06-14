b90696587cc2f8a35a72b74ac04fe03fdabf833f
AlyanTheCodingLegend <alyan.memon00@gmail.com>
2024-06-15 02:28:38 +0500

    Fixed methods to make the chat app fully functional

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { WebSocket, WebSocketServer } from 'ws';
-import { ChatManager } from './ChatManager';
+import { ChatManager } from './ChatManager.js';
 
 dotenv.config();
 
